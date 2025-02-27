c4a3a3a6b30eef273405bed4926773d799661632
Gopika-Rani <gopikarani1996@gmail.com>
2025-02-27 15:30:10 -0600

    intial commit

---
+++ 02-25-2025-gb-gobasics-v1.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+/*var i = 10
+
+func main() {
+
+	fmt.Println(i)
+}
+
+const i = "gopika"
+
+func main() {
+	//i= 30
+	fmt.Println(i)
+}
+*/
+var (
+	a int = 1
+	b     = 2
+	c     = "Gopika"
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
